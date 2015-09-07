0691bff1ced7300cecefd325a4b8778c45670846
GerodruS <gerodrus@gmail.com>
2015-09-07 17:58:01 +0300

    Gate works!

--- Assets/Gate.cs
+++ Assets/Gate.cs
@@ -6,19 +6,25 @@ public class Gate : MonoBehaviour
     public float force = 10000.0f;
 
     private HingeJoint hingeJoint;
-    private JointSpring jointSpring;
-    private Rigidbody rigidbody;
-
-    public void Hit()
-    {
-        rigidbody.AddForce(Vector3.forward * force);
-    }
 
     protected void Start()
     {
         hingeJoint = GetComponent<HingeJoint>();
-        rigidbody = GetComponent<Rigidbody>();
+    }
 
-        jointSpring = new JointSpring();
+    protected void FixedUpdate()
+    {
+        var spring = hingeJoint.spring;
+
+        if (Input.GetButton("Fire1"))
+        {
+            spring.spring = force;
+        }
+        else
+        {
+            spring.spring = 0.0f;
+        }
+
+        hingeJoint.spring = spring;
     }
 }
