a9b709f6edb8d85cee7eaf221a949005df92682d
GerodruS <gerodrus@gmail.com>
2015-09-07 19:16:55 +0300

    First hit object - cylinder

---
+++ Assets/HitObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HitObject : MonoBehaviour
+{
+    public float bounciness = 1.0f;
+    public PhysicMaterialCombine bounceCombine = PhysicMaterialCombine.Average;
+
+    protected void Start()
+    {
+        SetCollider();
+    }
+
+    protected void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        SetCollider();
+#endif
+    }
+
+    protected void SetCollider()
+    {
+        var collider = GetComponent<Collider>();
+        collider.material.bounciness = bounciness;
+        collider.material.bounceCombine = bounceCombine;
+        Debug.Log(collider.material.bounciness);
+    }
+}
